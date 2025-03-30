ca62bb2f11a7f5ad7af50baf736f89fec171ded5
re-side <gamingcaynt@gmail.com>
2025-03-31 02:46:32 +0300

    Add files via upload

--- ALG/lab2/Simpson.cpp
+++ ALG/lab2/Simpson.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
-double upperY(double x) {
-    return min(-2 * x + 12, 2 * x - 4);
+inline double upperY(double x) {
+    return 2 - fabs(x);
 }
 
-double lowerY(double x) {
-    return max(2 * x - 12, -2 * x + 4);
+inline double lowerY(double x) {
+    return -upperY(x);
 }
 
 double simpson2D(int n) {
-    double a_x = 4.0;
-    double b_x = 6.0;
-    double h_x = (b_x - a_x) / n;
-    double totalArea = 0.0;
+    double a = -2.0, b = 2.0;
+    double h = (b - a) / n;
+    double integral = 0.0;
 
-    for (int i = 0; i <= n; ++i) {
-        double x = a_x + i * h_x;
-        double a_y = lowerY(x);
-        double b_y = upperY(x);
-        if (a_y >= b_y) continue;
-        totalArea += (b_y - a_y) * h_x;
+    for (int i = 0; i <= n; i++) {
+        double x = a + i * h;
+        double y1 = upperY(x);
+        double y2 = lowerY(x);
+        integral += (y1 - y2) * (i == 0 || i == n ? 1 : (i % 2 == 0 ? 2 : 4));
     }
-    return totalArea;
+
+    integral *= (h / 3.0);
+    return integral;
 }
 
 int main() {
-    setlocale(LC_ALL, "Russian");
     int n = 1000;
-    cout << "Метод Симпсона: " << simpson2D(n) << endl;
+    double result = simpson2D(n);
+    cout << "Площадь фигуры методом Симпсона: " << result << endl;
     return 0;
 }

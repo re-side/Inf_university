0090830c5acf46c038d563c8b34b530ff5c6d3d5
re-side <gamingcaynt@gmail.com>
2025-05-14 21:51:51 +0300

    Add files via upload

--- ALG/lab3/main.cpp
+++ ALG/lab3/main.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-#ifdef _WIN32
-#include <windows.h>
-#endif
-using namespace std;
+#include <random>
+#include <iomanip>
 
-// Функция для обмена элементов
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
+using namespace std;
 
 // Сортировка пузырьком
 void bubbleSort(vector<int>& arr) {
@@ -27,37 +20,21 @@ void bubbleSort(vector<int>& arr) {
 
 // Сортировка слиянием
 void merge(vector<int>& arr, int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    vector<int> L(n1), R(n2);
-
-    for (int i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] < R[j]) {
-            arr[k] = L[i];
-            i++;
+    vector<int> temp(arr.size());
+    int i = left, j = mid + 1, k = left;
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k] = arr[i++];
         }
         else {
-            arr[k] = R[j];
-            j++;
+            temp[k] = arr[j++];
         }
         k++;
     }
-
-    while (i < n1) {
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2) {
-        arr[k] = R[j];
-        j++;
-        k++;
+    while (i <= mid) temp[k++] = arr[i++];
+    while (j <= right) temp[k++] = arr[j++];
+    for (int p = left; p <= right; p++) {
+        arr[p] = temp[p];
     }
 }
 
@@ -92,48 +69,64 @@ void quickSort(vector<int>& arr, int low, int high) {
     }
 }
 
+// Генерация случайного массива
+vector<int> generateRandomArray(int size) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dis(1, 10000);
+    vector<int> arr(size);
+    for (int i = 0; i < size; i++) {
+        arr[i] = dis(gen);
+    }
+    return arr;
+}
+
+// Замеры времени
+double measureTime(void (*sortFunc)(vector<int>&, int, int), vector<int>& arr) {
+    auto start = chrono::high_resolution_clock::now();
+    sortFunc(arr, 0, arr.size() - 1);
+    auto end = chrono::high_resolution_clock::now();
+    return chrono::duration<double, milli>(end - start).count();
+}
+
+double measureTimeBubble(vector<int>& arr) {
+    auto start = chrono::high_resolution_clock::now();
+    bubbleSort(arr);
+    auto end = chrono::high_resolution_clock::now();
+    return chrono::duration<double, milli>(end - start).count();
+}
+
 int main() {
-    // Настройка кодировки для Windows
-#ifdef _WIN32
-    SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);
-#endif
+    vector<int> sizes = { 1000, 5000, 10000, 50000 };
+    cout << fixed << setprecision(3);
+    cout << "Размер массива | Пузырьком (мс) | Слиянием (мс) | Быстрая (мс)\n";
+    cout << "---------------|---------------|--------------|-------------\n";
 
-    // Тестовый массив
-    vector<int> arr = { 64, 34, 25, 12, 22, 11, 90 };
-    cout << "Исходный массив: ";
-    for (int x : arr) cout << x << " ";
-    cout << endl;
+    for (int size : sizes) {
+        double bubbleTime = 0, mergeTime = 0, quickTime = 0;
+        int runs = 10;
 
-    // Сортировка пузырьком
-    vector<int> arr_bubble = arr;
-    auto start_bubble = chrono::high_resolution_clock::now();
-    bubbleSort(arr_bubble);
-    auto end_bubble = chrono::high_resolution_clock::now();
-    auto bubble_time = chrono::duration_cast<chrono::nanoseconds>(end_bubble - start_bubble).count();
-    cout << "Сортировка пузырьком: ";
-    for (int x : arr_bubble) cout << x << " ";
-    cout << "(Время: " << bubble_time << " нс)" << endl;
+        for (int i = 0; i < runs; i++) {
+            vector<int> arr = generateRandomArray(size);
 
-    // Сортировка слиянием
-    vector<int> arr_merge = arr;
-    auto start_merge = chrono::high_resolution_clock::now();
-    mergeSort(arr_merge, 0, arr_merge.size() - 1);
-    auto end_merge = chrono::high_resolution_clock::now();
-    auto merge_time = chrono::duration_cast<chrono::nanoseconds>(end_merge - start_merge).count();
-    cout << "Сортировка слиянием: ";
-    for (int x : arr_merge) cout << x << " ";
-    cout << "(Время: " << merge_time << " нс)" << endl;
+            vector<int> arrCopy = arr;
+            bubbleTime += measureTimeBubble(arrCopy);
 
-    // Быстрая сортировка
-    vector<int> arr_quick = arr;
-    auto start_quick = chrono::high_resolution_clock::now();
-    quickSort(arr_quick, 0, arr_quick.size() - 1);
-    auto end_quick = chrono::high_resolution_clock::now();
-    auto quick_time = chrono::duration_cast<chrono::nanoseconds>(end_quick - start_quick).count();
-    cout << "Быстрая сортировка: ";
-    for (int x : arr_quick) cout << x << " ";
-    cout << "(Время: " << quick_time << " нс)" << endl;
+            arrCopy = arr;
+            mergeTime += measureTime(mergeSort, arrCopy);
 
+            arrCopy = arr;
+            quickTime += measureTime(quickSort, arrCopy);
+        }
+
+        bubbleTime /= runs;
+        mergeTime /= runs;
+        quickTime /= runs;
+
+        cout << setw(14) << size << " | "
+            << setw(13) << bubbleTime << " | "
+            << setw(12) << mergeTime << " | "
+            << setw(11) << quickTime << "\n";
+    }
     return 0;
 }
